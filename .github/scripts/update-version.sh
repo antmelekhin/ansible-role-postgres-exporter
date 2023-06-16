ed34202fcbe055cb45ede3211f3d839f949a23ac
Melekhin Anton <antmelekhin@gmail.com>
2023-06-16 21:50:51 +0400

    ci: add `CURRENT_VERSION` and `LATEST_VERSION` variables, add functionality for README update

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -13,20 +13,25 @@ NO_COLOR='\e[0m'
 # Get latest version
 SOFTWARE="$1"
 URL="https://api.github.com/repos/prometheus-community/${SOFTWARE}/releases/latest"
-VERSION="$(curl --silent $URL | jq '.tag_name' | tr -d '"v')"
+LATEST_VERSION="$(curl --silent $URL | jq '.tag_name' | tr -d '"v')"
 
-if grep "_version: '${VERSION}'" 'defaults/main.yml'; then
+# Get current version
+CURRENT_VERSION="$(awk '/^*_version:/{print $2}' 'defaults/main.yml' | tr -d \')"
+
+# Compare current and latest versions
+if [ $CURRENT_VERSION == $LATEST_VERSION ]; then
     echo -e "${GREEN}Newest version is used.${NO_COLOR}"
     exit 0
 fi
 
 # Update latest version
-sed -i "s/_version:.*$/_version: '${VERSION}'/" 'defaults/main.yml'
+sed -i "s/_version:.*$/_version: '${LATEST_VERSION}'/" 'defaults/main.yml'
+sed -i "s/${CURRENT_VERSION}/${LATEST_VERSION}/" 'README.md'
 
 # Repository variables
 REPO_NAME=$(git config --get remote.origin.url | sed -e 's|^https://github.com/||')
-UPDATE_VERSION_BRANCH="update-to-${VERSION}"
-UPDATE_VERSION_COMMIT="fix(version): ${SOFTWARE} updated to \`${VERSION}\` release"
+UPDATE_VERSION_BRANCH="update-to-${LATEST_VERSION}"
+UPDATE_VERSION_COMMIT="fix(version): ${SOFTWARE} updated to \`${LATEST_VERSION}\` release"
 
 # Create an update branch
 REMOTE_BRANCH="$(curl --silent https://api.github.com/repos/${REPO_NAME}/branches/${UPDATE_VERSION_BRANCH} | jq -r .name)"
@@ -50,7 +55,7 @@ else
 fi
 
 # Create Pull Request
-PR_MESSAGE="The upstream [${SOFTWARE}](https://github.com/prometheus-community/${SOFTWARE}/releases) released new software version - **${VERSION}**!\n\nThis automated PR updates code to bring new version into repository."
+PR_MESSAGE="The upstream [${SOFTWARE}](https://github.com/prometheus-community/${SOFTWARE}/releases) released new software version - **${LATEST_VERSION}**!\n\nThis automated PR updates code to bring new version into repository."
 JSON="$(printf '{"title":"%s","body":"%s","head":"%s","base":"%s"}' "${UPDATE_VERSION_COMMIT}" "${PR_MESSAGE}" "${UPDATE_VERSION_BRANCH}" "main")"
 
 curl -L \
